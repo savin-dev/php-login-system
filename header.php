ff01569ac8b1e861ccd198b062ec18a15903ac9e
savin-dev <niduwarafonseka@gmail.com>
2020-12-29 01:08:31 +0530

    added a logo

--- header.php
+++ header.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <a href="#">
+                <img src = "image/logo.png" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Protfolio</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <form action="">
+                </form>
+            </div>
+        </nav>
+    </header>
 </body>
 </html>
